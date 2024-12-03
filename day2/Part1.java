82b28b904760b5bf286974ae7385a2d21b1704ff
Robert Kenney <kenneyrobert109@gmail.com>
2024-12-03 11:46:43 -0500

    Day 2 Part 1

---
+++ day2/Part1.java
@@ -0,0 +1,61 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Part1 {
+    public static void main(String[] args) {
+        String fileName = "../inputs/day2.txt";
+        try {
+            File file = new File(fileName);
+            Scanner fileScanner = new Scanner(file);
+
+            int safe = 0;
+            while (fileScanner.hasNextLine()) {
+                boolean descending = false;
+
+                String line = fileScanner.nextLine();
+
+                Scanner lineScanner = new Scanner(line);
+
+                int prev;
+                if (lineScanner.hasNextInt()) {
+                    prev = lineScanner.nextInt();
+                    safe++;
+                } else {
+                    continue;
+                }
+
+                int curr;
+                if (lineScanner.hasNextInt()) {
+                    curr = lineScanner.nextInt();
+                } else {
+                    continue;
+                }
+                if (Math.abs(curr - prev) > 3 || curr == prev) {
+                    safe--;
+                    continue;
+                }
+
+                descending = curr < prev;
+                while (lineScanner.hasNextInt()) {
+                    prev = curr;
+                    curr = lineScanner.nextInt();
+                    if (descending && curr > prev) {
+                        safe--;
+                        break;
+                    } else if (!descending && prev > curr) {
+                        safe--;
+                        break;
+                    } else if (Math.abs(curr - prev) > 3 || prev == curr) {
+                        safe--;
+                        break;
+                    }
+                }
+            }
+            System.out.println(safe);
+        } catch (FileNotFoundException e) {
+            System.out.println("Error opening file: " + fileName);
+        }
+    }
+}
